b6fce4e87ecd16661fa54a5c2974bb8d86fa7ac5
thasleemkarishma <thasleem1011@gmail.com>
2025-01-20 12:16:48 +0530

    Method added

--- src/com/programs/Student.java
+++ src/com/programs/Student.java
@@ -1,5 +1,15 @@
 package com.programs;
 
 public class Student {
+	
+	private String name;
+	private int id;
+	private String address;
+	
+	
+	public void writeExam() {
+		System.out.println("Writting....");
+	}
+	
 
 }
